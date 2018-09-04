c5dfe10ae66d9f00d7b90236a2035502c5a59c69
Iver Dihle Skjervum <iver.dihle.skjervum@blankoslo.no>
2018-09-04 13:35:09 +0200

    Rewrite to create-react-app

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Main from "main";
-
-require("../styles/main.less");
-
-ReactDOM.render(<Main />, document.getElementById('app'));
--- src/main.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-class Main extends React.Component {
-}
-export default Main;
